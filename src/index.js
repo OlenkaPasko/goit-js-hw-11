c55e7eb3e72ec7416c9a1062ed89804a54697947
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-04-07 00:41:39 +0300

    function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { getPhotos } from './js/apiPixabay';
 
-
 const refs = {
   form: document.querySelector('.search-form'),
   btnLoadMore: document.querySelector('.load-more'),
@@ -9,15 +8,15 @@ const refs = {
 };
 
 refs.form.addEventListener('submit', onSearchForm);
-refs.btnLoadMore.addEventListener('click',onBtnLoad);
+refs.btnLoadMore.addEventListener('click', onBtnLoad);
 
 let searchQuerry = '';
-//let currentPage = 1;
+let currentPage = 1;
 
 //Форма пошуку
 function onSearchForm(event) {
   event.preventDefault();
-  searchQuerry = event.target.value.trim().toLowerCase();
+  searchQuerry = event.target.value.trim();
   if (searchQuerry === '') {
     refs.btnLoadMore.classList.add('is-hidden');
     Notify.failure('Enter something.');
@@ -33,6 +32,41 @@ function onSearchForm(event) {
   }
 }
 
-function onBtnLoad() {
-    fetchImage(url);
+async function fetchImage(url) {
+  //"We're sorry, but you've reached the end of search results."
+  try {
+    const cards = response.data;
+    refs.gallery.insertAdjacentHTML('beforeend');
+    currentPage += 1;
+    refs.btnLoadMore.classList.remove('is-hidden');
+    lightbox.refresh();
+    return cards;
+  } catch {
+    refs.btnLoadMore.classList.add('is-hidden');
+    Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
 }
+
+function onBtnLoad() {
+  fetchImage(url);
+}
+/*галерея і картка зображень
+ <div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div> */
