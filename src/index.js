a4d5c82197238bf3755efdd8b31ff9787f794124
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-04-06 21:07:31 +0300

    onSearchForm

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { getPhotos } from './js/apiPixabay';
+
+
+const refs = {
+  form: document.querySelector('.search-form'),
+  input: document.querySelector('[type="text"]'),
+  btnSearch: document.querySelector('[type="submit"]'),
+  btnLoadMore: document.querySelector('.load-more'),
+  gallery: document.querySelector('.gallery'),
+};
+
+//let searchQuerry = '';
+//let currentPage = 1;
+
+refs.form.addEventListener('submit')
+
+
+
+//Форма пошуку
+const onSearchForm = async function {
+
+}
+
