28af218783affef6b51a4b5949f66c026caa8eb2
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-04-07 21:44:36 +0300

    getPhotos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import Notiflix from 'notiflix';
 //import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { getPhotos } from './js/apiPixabay';
+import { getPhotos } from './js/apiPixabay.js';
+
+
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '335113425-894140f70267936d7d418e310';
+const url = `${BASE_URL}?key=${API_KEY}&q=${value}&type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
 
 const refs = {
   form: document.querySelector('.search-form'),
--- src/js/apiPixabay.js
+++ src/js/apiPixabay.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const getPhotos = async (value, page) => {
   const BASE_URL = 'https://pixabay.com/api';
   const API_KEY = '335113425-894140f70267936d7d418e310';
-  const url = `${BASE_URL}?key=${API_KEY}&q=${value}&type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
+  const url = `?key=${API_KEY}&q=${value}&type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
   return await axios.get(`${BASE_URL}${url}`).then(response => response.data);
-};
+}
