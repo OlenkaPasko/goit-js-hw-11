56098f8f0f23a351d6d5de473d9c032b199a22e1
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-04-08 09:51:11 +0300

    all

--- src/index.js
+++ src/index.js
@@ -2,11 +2,103 @@ import Notiflix from 'notiflix';
 import axios from 'axios';
 //import { Notify } from 'notiflix/build/notiflix-notify-aio';
 //import { getPhotos } from './js/apiPixabay.js';
+const API_KEY = '35113425-894140f70267936d7d418e310';
+const BASE_URL = 'https://pixabay.com/api/';
+const refs = {
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  btnLoadMor: document.querySelector('.load-more'),
+};
+
+let searchQuery = '';
+let currentPage = 1;
+
+refs.form.addEventListener('submit', onSearchForm);
+refs.btnLoadMor.addEventListener('click', onBtnLoad);
+
+function onSearchForm(event) {
+  currentPage = 1;
+  event.preventDefault();
+  refs.gallery.innerHTML = '';
+  searchQuery = event.currentTarget.elements.searchQuery.value.trim();
+  const url = `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`;
+  if (searchQuery === '') {
+    refs.btnLoadMor.classList.add('is-hidden');
+    Notiflix.Notify.failure('Enter something.');
+  } else {
+    fetchImage(url).then(cards => {
+      if (cards === 0) {
+        refs.btnLoadMor.classList.add('is-hidden');
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } 
+    });
+  }
+}
+async function fetchImage(url) {
+  try {
+    const response = await axios(url);
+    const cards = response.data;
+    refs.gallery.insertAdjacentHTML('beforeend', renderCards(cards));
+    currentPage += 1;
+    refs.btnLoadMor.classList.remove('is-hidden');
+    lightbox.refresh();
+    return cards;
+  } catch {
+    refs.btnLoadMor.classList.add('is-hidden');
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+}
+function onBtnLoad() {
+  const url = `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`;
+  fetchImage(url);
+}
+
+function renderCards(cards) {
+  return cards.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+<a class='gallery__link' href='${largeImageURL}'><img class='gallery__image' src="${webformatURL}" alt="${tags}" loading="lazy" width='360' height='260'/></a>
+<div class="info">
+  <p class="info-item">
+    <b>Likes:${likes}</b>
+  </p>
+  <p class="info-item">
+    <b>Views:${views}</b>
+  </p>
+  <p class="info-item">
+    <b>Comments:${comments}</b>
+  </p>
+  <p class="info-item">
+    <b>Downloads:${downloads}</b>
+  </p>
+</div>
+</div>`;
+      }
+    )
+    .join('');
+}
 
 
-const BASE_URL = 'https://pixabay.com/api';
+
+
+
+
+
+/*const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '335113425-894140f70267936d7d418e310';
-//const url = `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`;
 
 const refs = {
   form: document.querySelector('#search-form'),
@@ -25,25 +117,32 @@ let currentPage = 1;
 function onSearchForm(event) {
   currentPage = 1;
   event.preventDefault();
+  refs.gallery.innerHTML = '';
   searchQuery = event.currentTarget.searchQuery.value.trim();
   const url = `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`;
+
   if (searchQuery === '') {
-    refs.gallery.innerHTML = '';
     refs.btnLoadMore.classList.add('is-hidden');
     Notiflix.Notify.failure('Enter something.');
-
-  } else {
-    fetchImage(url).then(cards => {
-      if (cards.length === 0) {
-        refs.gallery.innerHTML = '';
-        refs.btnLoadMore.classList.add('is-hidden');
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-    });
+    return;
   }
-}
+  //const response = getPhotos(searchQuery, currentPage);
+  //currentHits = response.hits.length;
+
+  if (cards.total > 40) {
+    refs.btnLoadMore.classList.remove('is-hidden');
+  } else {
+    refs.btnLoadMore.classList.add('is-hidden');
+  }
+    if (cards.total === 0) {
+      refs.gallery.innerHTML = '';
+      //refs.btnLoadMore.classList.add('is-hidden');
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+  }
+
 
 //"We're sorry, but you've reached the end of search results."
 
@@ -67,10 +166,10 @@ async function fetchImage(url){
 function onBtnLoad() {
   const url = `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`;
   fetchImage(url);
-}
+}*/
 /*галерея і картка зображень*/
 
-function renderGallery(cards) {
+/*function renderGallery(cards) {
   const markup = cards
     .map(
       ({
@@ -104,4 +203,4 @@ function renderGallery(cards) {
     )
     .join('');
   refs.gallery.insertAdjacentHTML('beforeend', markup);
-}
+}*/
--- src/js/apiPixabay.js
+++ src/js/apiPixabay.js
@@ -1,7 +1,8 @@
 /*import axios from 'axios';
 
-export const getPhotos = async (value, page) => {
+export const getPhotos = async (searchQuery, currentPage) => {
   const BASE_URL = 'https://pixabay.com/api';
   const API_KEY = '335113425-894140f70267936d7d418e310';
-  const url = `?key=${API_KEY}&q=${value}&type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
-  return await axios.get(`${BASE_URL}${url}`).then(response => response.data);*/
+  //const url = `?key=${API_KEY}&q=${searchQuery}&type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`;
+  return await axios.get(`${BASE_URL}${url}`).then(response => response.data);
+};*/
